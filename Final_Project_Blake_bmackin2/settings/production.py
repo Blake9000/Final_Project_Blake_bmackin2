9e0e3a1c1314ff0f2dbd8cc608c77b54272bd44b
Blake9000 <blakealmackin@gmail.com>
2025-12-12 19:49:31 -0600

    Reformatting completed project

---
+++ Final_Project_Blake_bmackin2/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *
+
+DEBUG = False
+
+# Enter your PythonAnywhere account here in the allowed hosts.
+# Replace it with your name:
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bmackin2.pythonanywhere.com', 'bmackin2.web.illinois.edu']
